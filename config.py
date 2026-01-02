83566f172a66e3bea6171b239864eb3ebcd7e105
Akaash Pothacamury <68061991+Akaash1234@users.noreply.github.com>
2026-01-03 00:07:58 +0530

    Remove comments from config.py

--- config.py
+++ config.py
@@ -2,4 +2,3 @@ class SimulationConfig:
     def __init__(self, inflation_rate=0.02, tech_growth=0.01):
         self.inflation_rate = inflation_rate
         self.tech_growth = tech_growth
-        # self.debug = True # default
